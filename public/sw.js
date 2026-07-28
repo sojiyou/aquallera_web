6271ff7eac317ba6464c5de990c1e9727c2afe61
Sojiyou <sojodecaran200@gmail.com>
2026-07-28 23:39:50 +0800

    Fix stale page: network-first SW, skipWaiting, purge old cache, bump v1.2.0

--- public/sw.js
+++ public/sw.js
@@ -1,31 +1,22 @@
-const CACHE = 'aquallera-v1';
+const CACHE = 'aquallera-v2';
 
-self.addEventListener('install', (e) => {
+self.addEventListener('install', () => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
   e.waitUntil(
-    caches.open(CACHE).then((cache) => {
-      return cache.addAll(['/', '/login', '/signup', '/dashboard', '/admin', '/help']);
-    })
+    Promise.all([
+      clients.claim(),
+      caches.keys().then((keys) => {
+        return Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)));
+      }),
+    ])
   );
 });
 
 self.addEventListener('fetch', (e) => {
   e.respondWith(
-    caches.match(e.request).then((cached) => {
-      return cached || fetch(e.request).then((res) => {
-        if (res.ok && e.request.method === 'GET') {
-          const clone = res.clone();
-          caches.open(CACHE).then((cache) => cache.put(e.request, clone));
-        }
-        return res;
-      });
-    }).catch(() => caches.match('/'))
-  );
-});
-
-self.addEventListener('activate', (e) => {
-  e.waitUntil(
-    caches.keys().then((keys) => {
-      return Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)));
-    })
+    fetch(e.request).catch(() => caches.match(e.request))
   );
 });
--- src/version.js
+++ src/version.js
@@ -1,2 +1,2 @@
-const BUILD_VERSION = "1.1.0";
+const BUILD_VERSION = "1.2.0";
 export default BUILD_VERSION;
